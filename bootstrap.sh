760af34216c0179ac84503cff804aed08d98b34c
Ludovic Maître <ludovic.maitre@effervens.com>
2015-07-23 21:13:58 +0200

    Update the bootstrap shell to launch ansible after vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
+# Create keys folder
+mkdir keys
 # Create local cache folder for squid
 mkdir -p $HOME/.squid/dk1
 # Copy user public key to vagrant and then to guests
 cp $HOME/.ssh/id_rsa.pub keys/id_rsa.pub
+# Init the vms
+vagrant up
+# Provision the bootstrap vm
+ansible-playbook -i hosts site.yml -t bootstrap
+# Provision the other vms
+ansible-playbook -i hosts site.yml
